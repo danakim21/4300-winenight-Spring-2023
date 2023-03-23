1b7c46c6c467b7ca109189086f9b1e031b7bfa3c
Dana Kim <dk559@cornell.edu>
2023-03-23 15:40:05 -0400

    Add FlavorTypoCorrector using minimum edit distance

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from db import mysql_engine, MYSQL_DATABASE
 from helpers.FlavorKeywordsExtractor import FlavorKeywords
+from helpers.FlavorTypoCorrector import FlavorTypoCorrector
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
 mysql_engine.load_file_into_db()
@@ -30,5 +31,10 @@ def wine_search():
 # words = keyword_extractor.flavor_words
 # print(words)
 
+flavor_typo_corrector = FlavorTypoCorrector(3) 
+original_list = ["friuty", "brightt", "good", "swet", "raspberry", "woodiy", "coke", "dri", "spice", "newflavor"]
+new_list = flavor_typo_corrector.get_replaced_flavor_list(original_list)
+print(original_list)
+print(new_list)
+
 app.run(debug=True)
-# test 
